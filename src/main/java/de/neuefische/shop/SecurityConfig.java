be57bc47f401c30faf5a1344c1f92f77b58aced2
André Schreck <moin@andreschreck.de>
2022-03-15 13:16:20 +0100

    cors

--- src/main/java/de/neuefische/shop/SecurityConfig.java
+++ src/main/java/de/neuefische/shop/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors().disable().authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .antMatchers("/**").authenticated()
                 .and().formLogin()
                 .and().httpBasic();
